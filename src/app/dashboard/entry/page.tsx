86dcce24f86fdfb95d6103501947fad747eb7a00
Malak Saad <malaksaadsalam@gmail.com>
2025-12-16 22:25:49 +0500

    fix: do some fixes

--- src/app/dashboard/entry/page.tsx
+++ src/app/dashboard/entry/page.tsx
@@ -1,16 +1,15 @@
 import PrayerForm from "@/components/PrayerForm";
 import {auth} from "@/auth";
 import DiaryForm from "@/components/DiaryForm";
-import {Separator} from "@/components/ui/separator";
 
 
 const EntryForm = async () => {
     const session = await auth();
-  return (
-    <div className={"flex flex-col md:flex-row px-4 gap-6 mb-3"}>
-      <PrayerForm session={session} />
-      <DiaryForm session={session} />
-    </div>
-  )
+    return (
+        <div className={"flex flex-col md:flex-row px-4 gap-6 mb-3"}>
+            <PrayerForm session={session}/>
+            <DiaryForm session={session}/>
+        </div>
+    )
 }
 export default EntryForm;

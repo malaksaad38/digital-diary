b3048fd92cd28a3943fe1316277fc2ab7f2ae366
Malak Saad <malaksaadsalam@gmail.com>
2025-12-30 23:07:05 +0500

    refactor: align diary and entry page

--- src/app/dashboard/diary/page.tsx
+++ src/app/dashboard/diary/page.tsx
@@ -4,7 +4,7 @@ import DiaryListClient from "@/components/diary/DiaryListClient";
 export default function PrayerDiaryPage() {
   return (
     <div className="min-h-screen bg-background">
-      <div className="max-w-6xl mx-auto px-4 space-y-4 sm:space-y-6 pt-6 sm:pt-14 pb-6">
+      <div className="max-w-6xl mx-auto px-4 space-y-4 sm:space-y-6  sm:pt-14 pb-6 pt-4">
         <DiaryListClient />
       </div>
     </div>
--- src/app/dashboard/entry/page.tsx
+++ src/app/dashboard/entry/page.tsx
@@ -6,7 +6,7 @@ import DiaryForm from "@/components/DiaryForm";
 const EntryForm = async () => {
     const session = await auth();
     return (
-        <div className="flex flex-col min-[975px]:flex-row gap-6 px-4 mb-3"
+        <div className="flex flex-col min-[975px]:flex-row gap-6 px-4 mb-3 pt-4 md:pt-8"
         >
             <PrayerForm session={session}/>
             <DiaryForm session={session}/>

290d1b3c8aa02c559b4cc51b00f55a3e44343bc5
Malak Saad <malaksaadsalam@gmail.com>
2026-01-16 22:54:39 +0500

    refactor: logout button

--- src/components/ButtonLogout.tsx
+++ src/components/ButtonLogout.tsx
@@ -1,25 +1,24 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import {signOut} from "next-auth/react";
 import {Button} from "@/components/ui/button";
 import {LogOutIcon} from "lucide-react";
-import {redirect} from "next/navigation";
 
 
 const ButtonLogout = () => {
-  return (
-      <Button
-        size="sm"
-        onClick={() => {
-            signOut()
-        }}
-        className={"w-full"}
-      >
-        <LogOutIcon/>
-        Logout
-      </Button>
+    return (
+        <Button
+            size="sm"
+            onClick={() => {
+                signOut()
+            }}
+            className={"w-full"}
+        >
+            <LogOutIcon/>
+            Logout
+        </Button>
 
-  );
+    );
 };
 
 export default ButtonLogout;
